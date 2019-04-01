6ee3ef8eaa79a583741c82dde62e5f9cceaced2c
Gabriel Andrade Cavalcante <gabrielc@lnxcit020030.cit>
2019-04-01 17:19:49 -0300

    FOI

--- SpringCloud/ZipkinServer/src/test/java/com/dojo/ZipkinServer/ZipkinServerApplicationTests.java
+++ SpringCloud/ZipkinServer/src/test/java/com/dojo/ZipkinServer/ZipkinServerApplicationTests.java
@@ -1,17 +1,6 @@
 package com.dojo.ZipkinServer;
 
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class ZipkinServerApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
 
 }
