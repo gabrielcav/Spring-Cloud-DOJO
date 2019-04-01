5e4d7a7dc05c470315616d18b9f2ccf22ebb8bbc
Gabriel Andrade Cavalcante <gabrielc@lnxcit020030.cit>
2019-04-01 14:30:20 -0300

    DOJO

--- SpringCloud/ZipkinServer/src/main/java/com/dojo/ZipkinServer/ZipkinServerApplication.java
+++ SpringCloud/ZipkinServer/src/main/java/com/dojo/ZipkinServer/ZipkinServerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableZipkinServer
 public class ZipkinServerApplication {
 
 	public static void main(String[] args) {
--- SpringCloud/ZuulServer/src/main/java/com/dojo/ZuulServer/ZuulServerApplication.java
+++ SpringCloud/ZuulServer/src/main/java/com/dojo/ZuulServer/ZuulServerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableZuulProxy
+@RestController
 public class ZuulServerApplication {
 
 	public static void main(String[] args) {
