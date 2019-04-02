1647d681ee42807bb2a6bc501f57147c1e8c313c
Gabriel Andrade Cavalcante <gabrielc@lnxcit020030.cit>
2019-04-02 11:00:07 -0300

    KKK

--- client-service/src/main/java/pl/piomin/microservices/client/api/Api.java
+++ client-service/src/main/java/pl/piomin/microservices/client/api/Api.java
@@ -1,5 +1,25 @@
 package pl.piomin.microservices.client.api;
 
+import org.springframework.web.bind.annotation.RestController;
+import pl.piomin.microservices.client.model.Client;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+@RestController
 public class Api {
+
+    private List<Client> clients;
+
+    protected Logger logger = Logger.getLogger(Api.class.getName());
+
+    public Api(){
+        clients = new ArrayList<>();
+        clients.add(new Client());
+    }
+//    public Api() {
+//        accounts = new ArrayList<>();
+//        accounts.add(new Account(1, 1, "111111"));
 	
 }
--- client-service/src/main/java/pl/piomin/microservices/client/model/Client.java
+++ client-service/src/main/java/pl/piomin/microservices/client/model/Client.java
@@ -8,4 +8,53 @@ public class Client {
     private String city;
     private Boolean isActive;
 
+    public Client () {}
+
+    public Client(Integer id, String address, String addressNumber, String city, Boolean isActive) {
+        this.id = id;
+        this.address = address;
+        this.addressNumber = addressNumber;
+        this.city = city;
+        this.isActive = isActive;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddressNumber() {
+        return addressNumber;
+    }
+
+    public void setAddressNumber(String addressNumber) {
+        this.addressNumber = addressNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
 }
